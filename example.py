7d9351ef3ee16fd49a75a079058e1ee6d34ec058
Salvatore Facista <sfacista@gmail.com>
2023-08-13 20:58:57 -0700

    testing stuff

--- example.py
+++ example.py
@@ -12,6 +12,21 @@
 # Outputs:
 ## Output 1:
 
-import pandas
+#import pandas
 import json
 import os
+import sys
+
+data=[]
+
+#with open('listingsAndReviews.json', encoding="utf8") as aFile:
+#    fileContents=aFile.read()
+
+aFile = open('listingsAndReviews.json', encoding="utf8")
+fileContents=aFile.read()
+
+#print(fileContents)
+
+aJson=json.loads(fileContents)
+
+print('This should work and hopefully we get to here!')
